f218733fc978423da14de58b3040b41313048e07
CronosVirus00 <141618736+CronosVirus00@users.noreply.github.com>
2023-08-09 20:37:07 +0100

    Update panterah-exchange.py

--- panterah-exchange.py
+++ panterah-exchange.py
@@ -8,33 +8,34 @@ st.markdown("<h1 style='text-align: center; color: pink;'>Panterah Exchange</h1>
 df = pd.read_csv('history-caffe.csv')
 df2 = pd.read_csv('history-ape.csv')
 
+ultima_riga = len(df)-1
 with st.container():
     col1, col2 = st.columns([6,2])
     with col1:
-        value = round(df['€'][0],2)
+        value = round(df['€'][ultima_riga],2)
         st.subheader(f':violet[Oggi un _caffé_ :coffee:  del Pantera vale:] {value}€')
         st.line_chart(df, x ='Giorno', y='€')
 
     with col2:    
-        value = str(round(df['€'][0],2)) + "€"
-        var = str(round(df['Var'][0],2)) + "%"
-        delta = round(df['Var'][0]-df['Var'][1],2)
-        st.metric('Variazione', value=var, delta=delta)
+        value = str(round(df['€'][ultima_riga],2)) + "€"
+        var = str(round(df['€'][ultima_riga]-df['€'][ultima_riga-1],2)) + "€"
+        delta = str(round(df['Delta'][ultima_riga],2)) + "%"
+        st.metric('Variazione', value=delta, delta=var)
 
 #####
 with st.container():
     col1, col2 = st.columns([6,2])
     with col1:
-        value2 = round(df2['€'][0],2)
+        value2 = round(df2['€'][ultima_riga],2)
         st.subheader(f':orange[Oggi un _aperitivo_ :cocktail: del Pantera vale:] {value2}€')
         st.line_chart(df2, x ='Giorno', y='€')
 
     with col2:    
-        value2 = str(round(df2['€'][0],2)) + "€"
-        var2 = str(round(df2['Var'][0],2)) + "%"
-        delta2 = round(df2['Var'][0]-df2['Var'][1],2)
-        st.metric('Variazione', value=var2, delta=delta2)
+        value2 = str(round(df2['€'][ultima_riga],2)) + "€"
+        var2 = str(round(df2['€'][ultima_riga]-df2['€'][ultima_riga-1],2)) + "€"
+        delta2 = str(round(df2['Delta'][ultima_riga],2)) + "%"
+        st.metric('Variazione', value=delta2, delta=var2)
 
 
-
-st.text(f'Ultimo Aggiornamento {df['Giorno'][len(df)]}')
+ultimo_aggiornamento = df['Giorno'][len(df)-1]
+st.text(f'Ultimo Aggiornamento {ultimo_aggiornamento}')
